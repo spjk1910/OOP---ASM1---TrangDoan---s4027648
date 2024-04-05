d841fb5c5de0a0a366d41efbfd1dffb636f791af
spjk1910 <trangdpt1910@gmail.com>
2024-04-05 21:26:05 +0700

    update Module folder (InsuranceCard | Claim)

--- src/Module/Claim.java
+++ src/Module/Claim.java
@@ -1,4 +1,19 @@
 package Module;
 
-public class Claim {
+import Utils.Date;
+
+import java.util.ArrayList;
+
+public class Claim
+{
+    private String id;
+    private Date claimDate;
+    private String insuredPerson;
+    private String cardNumber;
+    private Date examDate;
+    private ArrayList<String> documents;
+    private ClaimStatus status;
+    private ReceiverBankingInfo receiverBankingInfo;
+
+
 }
--- src/Module/InsuranceCard.java
+++ src/Module/InsuranceCard.java
@@ -1,4 +1,19 @@
 package Module;
 
-public class InsuranceCard {
+import Utils.Date;
+
+public class InsuranceCard
+{
+    private String cardNumber;
+    private String cardHolder;
+    private String policyOwner;
+    Date expirationDate;
+
+    public InsuranceCard(String cardNumber, String cardHolder, String policyOwner, Date expirationDate)
+    {
+        this.cardNumber = cardNumber;
+        this.cardHolder = cardHolder;
+        this.policyOwner = policyOwner;
+        this.expirationDate = expirationDate;
+    }
 }
