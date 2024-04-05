fa78fa253c1c564b24ad5136dff89c5316b34130
spjk1910 <trangdpt1910@gmail.com>
2024-04-05 21:43:09 +0700

    update Utils folder (Date)

--- src/Utils/Date.java
+++ src/Utils/Date.java
@@ -2,4 +2,21 @@ package Utils;
 
 public class Date
 {
+    private int day;
+    private int month;
+    private  int year;
+    public  Date(String date)
+    {
+        String[] tmp = date.split("/");
+
+        day = Integer.parseInt(tmp[0]);
+        month = Integer.parseInt(tmp[1]);
+        year = Integer.parseInt(tmp[2]);
+    }
+    public Date(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
 }
