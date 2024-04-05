7f70278fec7564038a00d2fdab350ee76912f50b
spjk1910 <trangdpt1910@gmail.com>
2024-04-05 23:49:58 +0700

    update Claim class

--- src/Module/Claim.java
+++ src/Module/Claim.java
@@ -16,5 +16,18 @@ public class Claim
     private ClaimStatus status;
     private ReceiverBankingInfo receiverBankingInfo;
 
-
+    public Claim(String id, Date claimDate, String insuredPerson, String cardNumber,
+                 Date examDate, ArrayList<String> documents, double claimAmount,
+                 ClaimStatus status, ReceiverBankingInfo receiverBankingInfo)
+    {
+        this.id = id;
+        this.claimDate = claimDate;
+        this.insuredPerson = insuredPerson;
+        this.cardNumber = cardNumber;
+        this.examDate = examDate;
+        this.documents = documents;
+        this.claimAmount = claimAmount;
+        this.status = status;
+        this.receiverBankingInfo = receiverBankingInfo;
+    }
 }
